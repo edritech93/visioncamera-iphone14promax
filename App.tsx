c09bad2f0851c02c795e02bcd21dc13f64b09775
Yudi Alviska <yudi.a@weefer.co.id>
2024-08-01 09:43:39 +0700

    add react-native-worklets-core

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet} from 'react-native';
-import {Camera, useCameraDevice} from 'react-native-vision-camera';
+import {
+  Camera,
+  Frame,
+  useCameraDevice,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
 
 export default function App() {
   const device = useCameraDevice('front');
@@ -14,9 +19,23 @@ export default function App() {
     _loadPermission();
   }, []);
 
+  const frameProcessor = useFrameProcessor((frame: Frame) => {
+    'worklet';
+    console.log(
+      new Date().toTimeString(),
+      `Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`,
+    );
+  }, []);
+
   if (device && hasPermission) {
     return (
-      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        pixelFormat={'rgb'}
+        frameProcessor={frameProcessor}
+      />
     );
   } else {
     return null;
