9748a3c1354556d926ffa7bf5b4d8d2dc1eb2258
Yudi Alviska <yudi.a@weefer.co.id>
2024-08-06 10:33:44 +0700

    add react-native-vision-camera-face-detector

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,75 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {
-  Camera,
   Frame,
   useCameraDevice,
-  useFrameProcessor,
+  Camera as VisionCamera,
 } from 'react-native-vision-camera';
+import {
+  Face,
+  Camera,
+  FaceDetectionOptions,
+} from 'react-native-vision-camera-face-detector';
 
 export default function App() {
+  const faceDetectionOptions = useRef<FaceDetectionOptions>({
+    // detection options
+  }).current;
+
   const device = useCameraDevice('front');
   const [hasPermission, setHasPermission] = useState(false);
+  const [arrayFace, setArrayFace] = useState<Face[]>([]);
 
   useEffect(() => {
     async function _loadPermission() {
-      const cameraPermission = await Camera.requestCameraPermission();
+      const cameraPermission = await VisionCamera.requestCameraPermission();
       setHasPermission(cameraPermission === 'granted');
     }
     _loadPermission();
   }, []);
 
-  const frameProcessor = useFrameProcessor((frame: Frame) => {
-    'worklet';
+  function handleFacesDetection(faces: Face[], frame: Frame) {
     console.log(
       new Date().toTimeString(),
-      `Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`,
+      'faces',
+      faces.length,
+      'frame',
+      frame.toString(),
     );
-  }, []);
+    setArrayFace(faces);
+  }
 
   if (device && hasPermission) {
     return (
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        pixelFormat={'rgb'}
-        frameProcessor={frameProcessor}
-      />
+      <View style={styles.container}>
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          pixelFormat={'rgb'}
+          faceDetectionCallback={handleFacesDetection}
+          faceDetectionOptions={faceDetectionOptions}
+        />
+        <Text style={styles.textFace}>{`Face: ${arrayFace.length}`}</Text>
+      </View>
     );
   } else {
     return null;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textFace: {
+    fontSize: 24,
+    lineHeight: 28,
+    color: 'red',
+    position: 'absolute',
+    bottom: 400,
+    left: 16,
+    right: 16,
+    textAlign: 'center',
+  },
+});
